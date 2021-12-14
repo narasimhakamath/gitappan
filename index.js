73e4047e791f460caf9f0c26f838ba52e728c9c2
Narasimha Kamath <narasimhakamath@outlook.com>
2021-12-14 15:54:42 +0530

    Added two new functions: getSupportedVendorList() and getBranches(repositoryURL).

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const fs = require('fs');
+
 const getVendor = (repositoryURL) => {
 	let platform;
 
@@ -12,4 +15,21 @@ const getVendor = (repositoryURL) => {
 	platform = platform.split('.')[0];
 
 	return platform;
+}
+
+const getSupportedVendorList = () => {
+	const directoryPath = path.join(__dirname, 'vendors');
+	const vendorJSFiles = fs.readdirSync(directoryPath);
+	const vendors = vendorJSFiles.map(vendorFile => vendorFile.split('.')[0]);
+
+	console.log(vendors);
+	return vendors;
+}
+
+const getBranches = (repositoryURL) => {
+	const vendor = getVendor(repositoryURL);
+	
+	if(!getSupportedVendorList().includes(vendor)) {
+		return "Oops! The vendor is not supported.";
+	}
 }
